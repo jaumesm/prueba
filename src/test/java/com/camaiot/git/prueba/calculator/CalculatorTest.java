fa9a0c5ca381bb5c95e2d53d402ca5c409270caf
Jaume <jaumesm@gmail.com>
2020-01-10 00:18:59 +0100

    add test

--- src/test/java/com/camaiot/git/prueba/calculator/CalculatorTest.java
+++ src/test/java/com/camaiot/git/prueba/calculator/CalculatorTest.java
@@ -2,16 +2,24 @@ package com.camaiot.git.prueba.calculator;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.util.Assert;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
     @Before
     public void setUp() throws Exception {
+        calculator = new Calculator();
     }
 
     @Test
     public void add() throws Exception {
+        Double actual = calculator.add(Double.valueOf(2), Double.valueOf(2));
+        Double expected = Double.valueOf(4);
+        assertEquals(expected, actual);
     }
 
     @Test
