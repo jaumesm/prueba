468f5a1c9a5bcb3e8fe892fd2955273a06171296
Jaume <jaumesm@gmail.com>
2020-01-10 00:13:33 +0100

    empty test

---
+++ src/test/java/com/camaiot/git/prueba/calculator/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.camaiot.git.prueba.calculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void substract() throws Exception {
+    }
+
+}
