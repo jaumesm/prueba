08c764624a3c8008df3e0e1e9036ac0905b1aabd
Jaume <jaumesm@gmail.com>
2020-01-10 00:11:00 +0100

    empty methods

--- src/main/java/com/camaiot/git/prueba/calculator/Calculator.java
+++ src/main/java/com/camaiot/git/prueba/calculator/Calculator.java
@@ -1,4 +1,14 @@
 package com.camaiot.git.prueba.calculator;
 
 public class Calculator {
+
+    public Double add(Double leftOperator, Double rightOperator){
+        // TODO
+        return null;
+    }
+
+    public Double substract(Double leftOperator, Double rightOperator){
+        // TODO
+        return null;
+    }
 }
